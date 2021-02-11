7f613670f54408fa1b6b3bc3f14cb6f399185520
Nathan Vegdahl <cessen@cessen.com>
2021-02-12 07:51:18 +0900

    Allow specifying separate passphrase length and number of generated words.
    This significantly reduces entropy, and is reflected in the printed
    entropy estimates.

--- passphrase.py
+++ passphrase.py
@@ -33,28 +33,30 @@ if __name__ == "__main__":
     wordlist_filename = sys.argv[1]
     passphrase_length = int(sys.argv[2])
 
-    percent_of_usable_words = 100
+    words_to_generate = passphrase_length
     if len(sys.argv) >= 4:
-        percent_of_usable_words = int(sys.argv[3])
+        words_to_generate = max(words_to_generate, int(sys.argv[3]))
 
     # Parse the word list file
     words = parse_wordlist_file(wordlist_filename)
     
-    apprx_word_list_length = len(words) * percent_of_usable_words // 100
+    effective_word_list_length = len(words) * passphrase_length // words_to_generate
 
     # Estimated entropy.
-    entropy = int(math.floor(math.log(apprx_word_list_length**passphrase_length, 2)))
+    entropy = int(math.floor(math.log(effective_word_list_length**passphrase_length, 2)))
 
     # Estimated entropy if the words are rearranged.
-    entropy_rearranged = int(math.floor(math.log(cmbn(apprx_word_list_length, passphrase_length), 2)))
+    entropy_rearranged = int(math.floor(math.log(cmbn(effective_word_list_length, passphrase_length), 2)))
 
     passphrase = ""
 
-    for i in range(0, passphrase_length):
+    for i in range(0, words_to_generate):
         passphrase += secrets.choice(words) + " "
 
     print(passphrase)
-    print("Word list size: " + str(len(words)))
-    print("Estimated entropy: " + str(entropy) + " bits")
-    print("Estimated entropy with rearrangement: " + str(entropy_rearranged) + " bits")
+    if passphrase_length < words_to_generate:
+        print("^^^ please select {} words ^^^\n".format(passphrase_length))
+    print("Source word list size: " + str(len(words)))
+    print("Estimated entropy with same word order:  " + str(entropy) + " bits")
+    print("Estimated entropy with rearrangement:    " + str(entropy_rearranged) + " bits")
 
